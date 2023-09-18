91b0c2411b26637229909e4ce1a44b753de8d786
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-09-18 23:30:07 +0200

    init commit

---
+++ src/Task.java
@@ -0,0 +1,65 @@
+public class Task {
+    public static void main(String[] args) {
+        figure1(6);
+        figure2(6);
+        figure3(6);
+        figure4(10);
+
+    }
+
+    public static void figure1(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || i == j || j == 1 || j == n) {
+                    System.out.print("*" + "\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void figure2(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || i == j || j == 1 || j == n || j == n - i + 1) {
+                    System.out.print("*" + "\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void figure3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n || j + i > n) {
+                    System.out.print("*" + "\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void figure4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || i == j || j == 1 || j == n || j == n - i + 1 || i == n - 1) {
+                    System.out.print("*" + "\t");
+                } else {
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
